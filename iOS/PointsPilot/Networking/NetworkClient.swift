908979fcbb0b2ad96c6ea68296afb0040f9c0a81
Dale Webster <dalewebster48@gmail.com>
2026-05-10 18:47:39 +0100

    Use a plist for networking base url

--- iOS/PointsPilot/Networking/NetworkClient.swift
+++ iOS/PointsPilot/Networking/NetworkClient.swift
@@ -61,6 +61,5 @@ enum NetworkError: Error {
 }
 
 extension URL {
-//    static var base: URL { URL(string: "http://localhost:4000")! }
-    static var base: URL { URL(string: "http://192.168.0.11:4000")! }
+    static var base: URL { PropertyListProvider.serverBaseURL }
 }
